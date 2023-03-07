af2df5a697a4e1a39ed7d327916912ed252b9159
HyeJiRoh <shgpwl509@gmail.com>
2023-03-08 02:30:02 +0900

    랜덤으로 부여되는 1~100까지의 숫자 맞추기 게임

--- What is this number?/code.py
+++ What is this number?/code.py
@@ -1,4 +1,28 @@
 import random
 
 num = random.randint(1, 100)
-print(num)
+count = 1
+
+print("==================GAME START==================")
+
+while True:
+    try:
+        print()
+        my_number = int(input("1~100까지의 숫자를 입력하세요 : "))
+
+        if my_number > num:
+            print("입력하신 숫자보다 작습니다.")
+        elif my_number < num:
+            print("입력하신 숫자보다 큽니다.")
+        else:
+            print()
+            print("⭐️정답입니다!⭐️")
+            print("당신은 %d번만에 맞추셨습니다."%count)
+            print()
+            print("===================GAME END===================")
+            break
+
+        count += 1
+
+    except:
+        print("에러가 발생했습니다. 정수만 입력 가능합니다.")
