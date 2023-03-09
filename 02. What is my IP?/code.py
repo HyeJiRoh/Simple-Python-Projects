60ad2c7c737bc12d6641aa8a775ddf8ad0060564
HyeJiRoh <shgpwl509@gmail.com>
2023-03-09 22:59:40 +0900

    내 컴퓨터의 내부, 외부 IP 조회하기

--- 02. What is my IP?/code.py
+++ 02. What is my IP?/code.py
@@ -5,8 +5,8 @@ import re
 in_addr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 in_addr.connect(("www.google.co.kr", 443))
 
-print("내부 IP:", in_addr.getsockname()[0])
+print("내 컴퓨터의 내부 IP:", in_addr.getsockname()[0])
 
 req = requests.get("http://ipconfig.kr")
 out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
-print("외부 IP:", out_addr)
+print("내 컴퓨터의 외부 IP:", out_addr)
